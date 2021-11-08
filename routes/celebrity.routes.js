b7bd864dad0e3bf053e0f58d39175302a1551288
Rahul Sandhu <sandhu.rahul25@gmail.com>
2021-11-08 11:48:07 +0530

    aaa

--- routes/celebrity.routes.js
+++ routes/celebrity.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const {getCelebrity} = require("../controllers/celebrity.controller");
+const {getCelebrity, addCelebrity} = require("../controllers/celebrity.controller");
 // import {auth} from '../middlewares/auth'
 // import {getMovie,getSub} from "../controllers/movie.controller"
 
 
 const router = express.Router();
 
+router.route("").post(addCelebrity);
 router.route("/:celeb_id").get(getCelebrity);
 // router.route("/subs/:tmdb_id/:language").get(getSub);
 
